61ea97388f881d0e1f4ec8fcf1ef9bafafd32fc2
Alex Tetervak <tetervak@sheridancollege.ca>
2024-11-19 18:48:34 -0500

    removed unused imports

--- app/src/main/java/com/example/todolist/AppRootScreen.kt
+++ app/src/main/java/com/example/todolist/AppRootScreen.kt
@@ -22,16 +22,12 @@ import android.content.res.Resources
 import android.os.Build
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.runtime.remember
